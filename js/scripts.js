c797373d79e0a69c8ecce1d99b776eaa00f36df5
HevellynOS <oshirohevellyn@gmail.com>
2023-04-10 10:54:12 -0300

    some
    somethings

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
+var coll = document.getElementsByClassName("nav_menu_collapsible");
+var i;
 
+for (i=0; i<coll.length; i++){
+    coll[i].addEventListener("click",function(){
+        this.classList.toggle("active");
+        var menu_itens = this.nextElementSibling;
+        if(menu_itens.style.display === "block"){
+            menu_itens.style.display = "none";
+
+        }else{
+            menu_itens.style.display = "block";
+        }
+    })
+}
